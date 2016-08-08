4af967b2760ee03b157de2fa1931196a110276dc
TiagoCF <13144415@stu.mmu.ac.uk>
2016-08-09 00:52:43 +0100

    camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,23 +5,25 @@ public class CameraFollow : MonoBehaviour {
 
     Transform player;
     public Vector3 camOffSet;
+	public float groundPos;
 	public float camvel = 0;
+	public float fixedCamPosOffset;
 
 	// Use this for initialization
 	void Start () {
+		
         player = GameObject.Find("Haro").transform;
-        camOffSet = new Vector3(0, 7, transform.position.z);
-
+        camOffSet = new Vector3(0, 1, transform.position.z);
+		groundPos = (float)0.9610672;
+	
     }
 
     // Update is called once per frame
     void Update () {
-
-		transform.position = new Vector3(player.position.x, -6/*player.position.y*/,0) + camOffSet;
-
-	
-
+		
+		fixedCamPosOffset = player.position.y - groundPos;
+		transform.position = new Vector3(player.position.x, player.position.y - (fixedCamPosOffset / 1.5f) ,0) +camOffSet;
+		//transform.position = new Vector3(player.position.x,-6/,0) + camOffSet;
 
     }
-
 }
