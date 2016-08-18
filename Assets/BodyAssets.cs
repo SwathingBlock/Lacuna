7628086f623f38a849b657dddb18ff228dead6df
TiagoCF <13144415@stu.mmu.ac.uk>
2016-08-18 14:25:44 +0100

    trigger platforms

--- Assets/BodyAssets.cs
+++ Assets/BodyAssets.cs
@@ -5,8 +5,10 @@ public class BodyAssets : MonoBehaviour {
 	public int Hstate;
 	// Use this for initialization
 	GameObject tie;
+	public Animator anim;
 	void Start () {
 		tie = GameObject.Find ("Tie");
+		anim = GameObject.Find ("Haro_Animation").GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -15,6 +17,8 @@ public class BodyAssets : MonoBehaviour {
 		if (tie.activeSelf) {
 			if (GameObject.Find ("Haro_Animation").GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Walk")) {
 				tie.GetComponent<Animator> ().SetInteger ("state", 1);
+
+				tie.GetComponent<Animator> ().Play ("tie", 0, anim.GetCurrentAnimatorStateInfo (0).normalizedTime);
 			} else {
 				tie.GetComponent<Animator> ().SetInteger ("state", 0);
 			}
--- Assets/triggerPlat.cs
+++ Assets/triggerPlat.cs
@@ -15,20 +15,20 @@ public class triggerPlat : MonoBehaviour {
 	
 		playerCollider = GameObject.Find ("Haro").GetComponent<BoxCollider2D> ();
 		Physics2D.IgnoreCollision (triggerCollider, platColl, true);
-		Physics2D.IgnoreCollision (playerCollider, platColl, true);
+
 		Debug.Log ("a");
 	}
 
-	
+
 	// Update is called once per frame
-	void onTriggerEnter2D(Collider2D other){
+	void onTriggerEnter(Collider2D other){
 		if (other.gameObject.name == "Haro") {
 		
 			Physics2D.IgnoreCollision (platColl, playerCollider, true);
 
 		}
 	}
-	void onTriggerExit2D (Collider2D other){
+	void onTriggerExit (Collider2D otyther){
 		Debug.Log("hey");
 		if (other.gameObject.name == "Haro") {
 
